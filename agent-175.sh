3c4e13ce471db8a3ad960acc0833b11c417a70af
moyu-hax <simplething168@outlook.com>
2025-01-12 16:23:05 +0800

    Update agent-175.sh

--- agent-175.sh
+++ agent-175.sh
@@ -94,7 +94,7 @@ echo -e "${green}开始修改nezha-agent服务"
 if [[ "${release}" == "alpine" ]]; then
     if [ -f /etc/init.d/nezha-agent ]; then
         echo -e "${green}/etc/init.d/nezha-agent服务存在,开始尝试禁用自动更新 ${plain}"
-        sed -i '/command_args=/ s/"$/ --disable-auto-update --disable-force-update"/' /etc/init.d/nezha-agent
+        sudo sed -i '/command_args=/ s/"$/ --disable-auto-update --disable-force-update"/' /etc/init.d/nezha-agent
         echo -e "${green}=========================================== ${plain}"
         cat /etc/init.d/nezha-agent  
     else
@@ -103,72 +103,4 @@ if [[ "${release}" == "alpine" ]]; then
 else
     if [ -f /etc/systemd/system/nezha-agent.service ]; then
         echo -e "${green}/etc/systemd/system/nezha-agent.service服务存在,开始尝试禁用自动更新${plain}"
-        sudo sed -i '/^ExecStart=/ s/$/ --disable-auto-update --disable-force-update/' /etc/systemd/system/nezha-agent.service
-        echo -e "${green}=========================================== ${plain}"   
-        cat /etc/systemd/system/nezha-agent.service
-    else
-        echo -e "${yellow}/etc/systemd/system/nezha-agent.service服务不存在,请尝试手动修改${plain}"
-    fi
-fi
-}
-
-Downlond_agent(){
-echo -e "${green}开始尝试降级Agent"
-    if [ -f /opt/nezha/agent/nezha-agent ]; then
-        echo -e "${green}=========================================== ${plain}"        
-        echo -e "${green}/opt/nezha/agent/nezha-agent存在,开始尝试降级到${VERSION}${plain}"
-        echo -e "${green}检测到系统为: ${release} 架构: ${arch} ${plain}"
-
-        if [[ "${arch}" == "amd64" ]]; then
-            wget https://github.com/nezhahq/agent/releases/download/v0.${VERSION}/nezha-agent_linux_amd64.zip && unzip nezha-agent_linux_amd64.zip && rm nezha-agent_linux_amd64.zip && mv nezha-agent /opt/nezha/agent/nezha-agent
-        elif [[ "${arch}" == "arm64" ]]; then
-            wget https://github.com/nezhahq/agent/releases/download/v0.${VERSION}/nezha-agent_linux_arm64.zip && unzip nezha-agent_linux_arm64.zip && rm nezha-agent_linux_arm64.zip && mv nezha-agent /opt/nezha/agent/nezha-agent
-        fi
-    else
-       echo -e "${yellow}/opt/nezha/agent/nezha-agent不存在,请尝试手动降级${plain}"
-    fi
-}
-
-restart_agent(){
-echo -e "${green}开始尝试重启agent服务${plain}"
-
-# 检查 systemctl 是否存在，并获取其路径
-if command -v systemctl >/dev/null 2>&1; then
-    systemctl_path=$(command -v systemctl)
-    echo -e "${green}检测到 systemctl 命令,路径为: ${systemctl_path}${plain}"
-    # 使用 systemctl 管理服务
-    if [[ "${release}" == "centos" ]] || [[ "${release}" == "debian" ]] || [[ "${release}" == "ubuntu" ]]; then
-        sudo "${systemctl_path}" daemon-reload
-        sudo "${systemctl_path}" restart nezha-agent
-        if [ $? -eq 0 ]; then
-            echo -e "${green}nezha-agent服务已成功重启\n${plain}"
-        else
-            echo -e "${red}nezha-agent服务重启失败\n${plain}"
-        fi
-    else
-        echo -e "${yellow}当前系统 ${release} 不支持 systemctl 管理服务,请尝试手动重启 nezha-agent 服务${plain}"
-    fi
-elif [[ "${release}" == "alpine" ]]; then
-    # Alpine 使用 rc-service
-    chmod +x /etc/init.d/nezha-agent
-    rc-update add nezha-agent default
-    rc-service nezha-agent restart
-    if [ $? -eq 0 ]; then
-        echo -e "${green}nezha-agent服务已成功重启\n${plain}"
-    else
-        echo -e "${red}nezha-agent服务重启失败\n${plain}"
-    fi
-else
-    echo -e "${yellow}未检测到 systemctl 命令,且系统不是 Alpine,请尝试手动重启 nezha-agent 服务${plain}"
-fi
-}
-
-echo -e "${green}当前系统为: ${release} 架构: ${arch} ${plain}"
-
-install_base
-Disable_automatic_updates
-sleep 1
-        echo -e "${green}=========================================== ${plain}" 
-        echo -e "${green}请自行判断配置是否正确 ${plain}" 
-Downlond_agent
-restart_agent
+        sudo sed -i '/^ExecStart=/ s/$/ --disable-auto-update --disable-force-upd▉
