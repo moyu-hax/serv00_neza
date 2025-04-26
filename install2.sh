976801d01e99081eca0f7d9bfba69fb5c69c9b13
moyu-hax <simplething168@outlook.com>
2025-04-26 13:28:50 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -369,14 +369,14 @@ install_agent() {
 
     # Nezha Monitoring Folder
     sudo mkdir -p $NZ_AGENT_PATH
-
+    echo "当前最新版本为： ${_version}"
     echo "正在下载监控端"
     if [ -z "$CN" ]; then
         NZ_AGENT_URL="https://${GITHUB_URL}/nezhahq/agent/releases/download/${_version}/nezha-agent_linux_${os_arch}.zip"
     else
         NZ_AGENT_URL="https://${GITHUB_URL}/naibahq/agent/releases/download/${_version}/nezha-agent_linux_${os_arch}.zip"
     fi
-
+    echo "当前最新版本为： ${_version}"
     _cmd="wget -t 2 -T 60 -O nezha-agent_linux_${os_arch}.zip $NZ_AGENT_URL >/dev/null 2>&1"
     if ! eval "$_cmd"; then
         err "Release 下载失败，请检查本机能否连接 ${GITHUB_URL}"
