0da4f084d8a11810371150b6769584641006236f
moyu-hax <simplething168@outlook.com>
2025-04-26 13:21:04 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -598,7 +598,7 @@ restart_and_update_standalone() {
     #     _version=$(curl -m 10 -sL "https://gitee.com/api/v5/repos/naibahq/nezha/releases/latest" | awk -F '"' '{for(i=1;i<=NF;i++){if($i=="tag_name"){print $(i+2)}}}')
     # fi
 
-    _version="v0.17.5"
+    _version="v0.20.13"
 
     if [ -z "$_version" ]; then
         err "获取 Dashboard 版本号失败，请检查本机能否链接 https://api.github.com/repos/naiba/nezha/releases/latest"
